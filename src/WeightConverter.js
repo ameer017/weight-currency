4ce6b098418eed5ffd8c3c70c0ce893efe0f6841
mac <mac@macs-MacBook-Air-2.local>
2023-08-28 07:21:24 +0100

    component updated

--- src/WeightConverter.js
+++ src/WeightConverter.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import './index.css'
-const WeightToCurrencyConverter = () => {
-  const [weight, setWeight] = useState(0);
-  const [conversionRate, setConversionRate] = useState(0);
-  const [convertedAmount, setConvertedAmount] = useState(0);
 
-  useEffect(() => {
-    // Fetch conversion rate from the API using your API key
-    const apiKey = '14b716abbf794520a95117eb4d4fd6f8';
-    const currencyCode = 'USD'; // Convert to US Dollars
+const WeightToCurrencyConverter = () => {
+    const [weight, setWeight] = useState(0);
+    const [conversionRates, setConversionRates] = useState({});
+    const [targetCurrency, setTargetCurrency] = useState('USD');
+    const [convertedAmount, setConvertedAmount] = useState(0);
+    
+    useEffect(() => {
+      const apiKey = '14b716abbf794520a95117eb4d4fd6f8';
+    // Fetch conversion rates from the API using your API key
     fetch(`https://openexchangerates.org/api/latest.json?app_id=${apiKey}`)
       .then(response => response.json())
       .then(data => {
         if (data.rates) {
-          setConversionRate(data.rates[currencyCode]);
+          setConversionRates(data.rates);
         }
       });
   }, []);
@@ -21,16 +21,29 @@ const WeightToCurrencyConverter = () => {
   const handleWeightChange = event => {
     const newWeight = parseFloat(event.target.value);
     setWeight(newWeight);
-    setConvertedAmount(newWeight * conversionRate); // Calculate the converted amount
+    setConvertedAmount(newWeight * conversionRates[targetCurrency]);
   };
-  
+
+  const handleCurrencyChange = event => {
+    const newCurrency = event.target.value;
+    setTargetCurrency(newCurrency);
+    setConvertedAmount(weight * conversionRates[newCurrency]);
+  };
 
   return (
-    <div className='App'>
+    <div>
       <h1>Weight to Currency Converter</h1>
       <label>Enter weight in lbs:</label>
       <input type="number" value={weight} onChange={handleWeightChange} />
-      <p>Converted amount in USD: {convertedAmount}</p>
+      <label>Select target currency:</label>
+      <select value={targetCurrency} onChange={handleCurrencyChange}>
+        {Object.keys(conversionRates).map(currency => (
+          <option key={currency} value={currency}>
+            {currency}
+          </option>
+        ))}
+      </select>
+      <p>Converted amount: {convertedAmount.toFixed(2)} {targetCurrency}</p>
     </div>
   );
 };
